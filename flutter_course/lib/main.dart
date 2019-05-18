e64ea79259848fab5941f25c8df2894863cade02
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-05-18 08:39:26 -0400

    Added first button

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -6,19 +6,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/food.jpg'),
-              Text('Food Paradise')
-            ],
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('EasyList'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              child: Text('Add Product'),
+              onPressed: () {},
+            ),
           ),
-        ),
-      )
-    );
+          Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/food.jpg'),
+                Text('Food Paradise')
+              ],
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
