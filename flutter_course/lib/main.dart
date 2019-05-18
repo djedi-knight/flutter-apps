a9f728cfcf2ff23b2edc06935b275e034719f485
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-05-17 23:10:43 -0400

    Completed build method

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 main() {
-  
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+      )
+    );
   }
 }
