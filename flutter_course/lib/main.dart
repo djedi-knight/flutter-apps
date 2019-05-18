ffd919f1886475cb3b5b51d18665976292856633
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-05-18 16:44:51 -0400

    Added iterable list of product to _MyAppState

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,14 +28,18 @@ class _MyAppState extends State<MyApp> {
               onPressed: () {},
             ),
           ),
-          Card(
-            child: Column(
-              children: <Widget>[
-                Image.asset('assets/food.jpg'),
-                Text('Food Paradise')
-              ],
-            ),
-          ),
+          Column(
+            children: _products
+                .map((element) => Card(
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset('assets/food.jpg'),
+                          Text(element)
+                        ],
+                      ),
+                    ))
+                .toList(),
+          )
         ],
       ),
     ));
