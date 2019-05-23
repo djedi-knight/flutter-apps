100e9d6cb00d40386fc62b01ab548895891c6b75
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-05-23 16:23:22 -0400

    Implemented WillPopScope

--- flutter_course/lib/pages/product.dart
+++ flutter_course/lib/pages/product.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
@@ -8,29 +9,36 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text(title),
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('Delete'),
-                onPressed: () => Navigator.pop(context, true),
+    return WillPopScope(
+      onWillPop: () {
+        print('Back button pressed');
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(imageUrl),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(title),
               ),
-            ),
-          ],
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('Delete'),
+                  onPressed: () => Navigator.pop(context, true),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
