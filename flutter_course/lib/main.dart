ebc7249456d7cc579793b6e89cc4dba81dd93b50
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-05-23 17:12:10 -0400

    Added AuthPage

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart';
 
-import './pages/home.dart';
+import './pages/auth.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.deepOrange,
           accentColor: Colors.deepPurple,
           brightness: Brightness.light),
-      home: HomePage(),
+      home: AuthPage(),
     );
   }
 }
