a2e5b0129a52c0ce44fef30a5f56cbe7c0a166d0
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-05-26 21:50:49 -0400

    Added/ decorated location to Products widget

--- flutter_course/lib/products.dart
+++ flutter_course/lib/products.dart
@@ -44,6 +44,21 @@ class Products extends StatelessWidget {
               ],
             ),
           ),
+          DecoratedBox(
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1.0,
+                ),
+                borderRadius: BorderRadius.circular(6.0)),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 6.0,
+                vertical: 2.5,
+              ),
+              child: Text('Sweet Revenge Bakery, Windsor, ON, Canada'),
+            ),
+          ),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
