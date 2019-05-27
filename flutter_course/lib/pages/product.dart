7e33aaab20a7e75d7e2309ec3b0c5bb4bd443c64
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-05-27 14:46:49 -0400

    Further refactored code

--- flutter_course/lib/pages/product.dart
+++ flutter_course/lib/pages/product.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/ui_elements/title_default.dart';
+
 class ProductPage extends StatelessWidget {
   final String title;
   final String imageUrl;
@@ -8,6 +9,35 @@ class ProductPage extends StatelessWidget {
   final String description;
 
   ProductPage(this.title, this.imageUrl, this.price, this.description);
+
+  Widget _buildAddressPriceRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Sweet Revenge Bakery, Windsor, ON, Canada',
+          style: TextStyle(
+            fontFamily: 'Oswald',
+            color: Colors.grey,
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 5.0),
+          child: Text(
+            '|',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        Text(
+          '\$' + price.toString(),
+          style: TextStyle(
+            fontFamily: 'Oswald',
+            color: Colors.grey,
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,35 +59,13 @@ class ProductPage extends StatelessWidget {
                 padding: EdgeInsets.all(10.0),
                 child: TitleDefault(title),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Sweet Revenge Bakery, Windsor, ON, Canada',
-                    style: TextStyle(
-                      fontFamily: 'Oswald',
-                      color: Colors.grey,
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 5.0),
-                    child: Text(
-                      '|',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ),
-                  Text(
-                    '\$' + price.toString(),
-                    style: TextStyle(
-                      fontFamily: 'Oswald',
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
+              _buildAddressPriceRow(),
               Container(
                 padding: EdgeInsets.all(10.0),
-                child: Text(description, textAlign: TextAlign.center,),
+                child: Text(
+                  description,
+                  textAlign: TextAlign.center,
+                ),
               ),
             ],
           ),
