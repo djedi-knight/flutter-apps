99a183333a3ee5292241a7a690362e1e78fb884b
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-05-27 10:08:50 -0400

    Refactored code to create ProductCard widget

--- flutter_course/lib/widgets/products/products.dart
+++ flutter_course/lib/widgets/products/products.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './price_tag.dart';
+import './product_card.dart';
 
 class Products extends StatelessWidget {
   final List<Map<String, dynamic>> products;
@@ -9,74 +9,12 @@ class Products extends StatelessWidget {
     print('[Products Widget] Constructor');
   }
 
-  Widget _buildProductItem(BuildContext context, int index) {
-    return Card(
-      child: Column(
-        children: <Widget>[
-          Image.asset(products[index]['image']),
-          Container(
-            padding: EdgeInsets.only(top: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  products[index]['title'],
-                  style: TextStyle(
-                    fontSize: 26.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Oswald',
-                  ),
-                ),
-                SizedBox(
-                  width: 8.0,
-                ),
-                PriceTag(products[index]['price'].toString()),
-              ],
-            ),
-          ),
-          DecoratedBox(
-            decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1.0,
-                ),
-                borderRadius: BorderRadius.circular(6.0)),
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 6.0,
-                vertical: 2.5,
-              ),
-              child: Text('Sweet Revenge Bakery, Windsor, ON, Canada'),
-            ),
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.info),
-                color: Theme.of(context).accentColor,
-                onPressed: () => Navigator.pushNamed<bool>(
-                      context,
-                      '/product/' + index.toString(),
-                    ),
-              ),
-              IconButton(
-                icon: Icon(Icons.favorite_border),
-                color: Colors.red,
-                onPressed: () => {},
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
   Widget _buildProductList() {
     Widget productCards;
     if (products.length > 0) {
       productCards = ListView.builder(
-        itemBuilder: _buildProductItem,
+        itemBuilder: (BuildContext context, int index) =>
+            ProductCard(products[index], index),
         itemCount: products.length,
       );
     } else {
