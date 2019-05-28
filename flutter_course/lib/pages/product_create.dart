4dafe20fa2605e7548e32204a93c25080cc278f7
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-05-27 22:09:35 -0400

    Added form validation to ProductCreate

--- flutter_course/lib/pages/product_create.dart
+++ flutter_course/lib/pages/product_create.dart
@@ -20,6 +20,11 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   Widget _buildTitleTextField() {
     return TextFormField(
       decoration: InputDecoration(labelText: 'Product Title'),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Title is required';
+        }
+      },
       onSaved: (String value) {
         setState(() {
           _titleValue = value;
@@ -53,6 +58,9 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   }
 
   void _submitForm() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
     _formKey.currentState.save();
     final Map<String, dynamic> product = {
       'title': _titleValue,
