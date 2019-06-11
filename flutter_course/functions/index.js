312e7357c28910167967cc9de65f179c7fef4f3f
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-06-11 14:52:03 -0400

    Created storeImage cloud function

--- flutter_course/functions/index.js
+++ flutter_course/functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +7,15 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.storeImage = functions.https.onRequest((request, response) => {
+  return cors(request, response, () => {
+    if (request.method !== 'POST') {
+      return response.status(500).json({ message: 'Not allowed.' });
+    }
+    if (!request.headers.authorization || !request.headers.authorization.startsWith('Bearer ')) {
+      return response.status(401).json({ error: 'Unauthorized.' });
+    }
+    let idToken = request.headers.authorization.split('Bearer ')[1];
+  });
+}); 
