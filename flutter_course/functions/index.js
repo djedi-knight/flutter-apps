ad10720b7e4a779d900ead80eefcb4ce9df8d992
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-06-11 16:09:43 -0400

    Finished storeImage firebase cloud function

--- flutter_course/functions/index.js
+++ flutter_course/functions/index.js
@@ -3,8 +3,22 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 
-const cors = require('cors')({ origin: true });
+const cors = require('cors')({
+  origin: true
+});
 const Busboy = require('busboy');
+const firebaseAdmin = require('firebase-admin');
+const uuid = require('uuid/v4');
+
+const gcconfig = {
+  projectId: 'fluttercourse-c2b8e',
+  keyFilename: 'config.json'
+};
+const gcs = require('@google-cloud/storage')(gcconfig);
+
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(require('./config.json'))
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -16,23 +30,33 @@ const Busboy = require('busboy');
 exports.storeImage = functions.https.onRequest((request, response) => {
   return cors(request, response, () => {
     if (request.method !== 'POST') {
-      return response.status(500).json({ message: 'Not allowed.' });
+      return response.status(500).json({
+        message: 'Not allowed.'
+      });
     }
 
     if (!request.headers.authorization || !request.headers.authorization.startsWith('Bearer ')) {
-      return response.status(401).json({ error: 'Unauthorized.' });
+      return response.status(401).json({
+        error: 'Unauthorized.'
+      });
     }
 
-    let idToken = request.headers.authorization.split('Bearer ')[1];
+    const idToken = request.headers.authorization.split('Bearer ')[1];
 
-    const busboy = new Busboy({ headers: request.headers });
+    const busboy = new Busboy({
+      headers: request.headers
+    });
 
     let uploadData;
     let oldImagePath;
 
     busboy.on('file', (fieldName, file, fileName, encoding, mimeType) => {
       const filePath = path.join(os.tmpdir(), fileName);
-      uploadData = { filePath: filePath, type: mimeType, name: fileName };
+      uploadData = {
+        filePath: filePath,
+        type: mimeType,
+        name: fileName
+      };
       file.pipe(fs.createWriteStream(filePath));
     });
 
@@ -40,6 +64,49 @@ exports.storeImage = functions.https.onRequest((request, response) => {
       oldImagePath = decodeURIComponent(value);
     });
 
-    busboy.on('finish', () => {});
+    busboy.on('finish', () => {
+      const bucket = gcs.bucket('fluttercourse-c2b8e.appspot.com');
+      const id = uuid();
+      let imagePath = 'images/' + id + '-' + uploadData.name;
+
+      if (oldImagePath) {
+        imagePath = oldImagePath;
+      }
+
+      return firebaseAdmin
+        .auth()
+        .verifyIdToken(idToken)
+        .then(decodedToken => {
+          return bucket.upload(uploadData.filePath, {
+            uploadType: 'media',
+            desination: imagePath,
+            metadata: {
+              metadata: {
+                contentType: uploadData.type,
+                firebaseStorageDownloadToken: id
+              }
+            }
+          });
+        })
+        .then(() => {
+          return response.statusCode(201).json({
+            imageUrl:
+              'https://firebasestorage.googleapis.com/v0/b/' +
+              bucket.name +
+              '/o/' +
+              encodeURIComponent(imagePath) +
+              '?alt=media&token=' +
+              id,
+            imagePath: imagePath
+          });
+        })
+        .catch(error => {
+          return response.statusCode(401).json({
+            error: 'Unauthorized.'
+          });
+        });
+    });
+    
+    return busboy.end(request.rawBody);
   });
 }); 
