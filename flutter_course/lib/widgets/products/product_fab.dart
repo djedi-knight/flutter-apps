82939c05a1bac2cc5768ba7627e3d0fd21b9782c
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-06-19 14:45:59 -0400

    Implemented the contact button

--- flutter_course/lib/widgets/products/product_fab.dart
+++ flutter_course/lib/widgets/products/product_fab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../models/scoped-models/main.dart';
 import '../../models/product.dart';
@@ -39,7 +40,14 @@ class _ProductFABState extends State<ProductFAB> {
                   Icons.mail,
                   color: Theme.of(context).primaryColor,
                 ),
-                onPressed: () {},
+                onPressed: () async {
+                  final url = 'mailto:${widget.product.userEmail}';
+                  if (await canLaunch(url)) {
+                    await launch(url);
+                  } else {
+                    throw 'Could not launch!';
+                  }
+                },
               ),
             ),
             Container(
