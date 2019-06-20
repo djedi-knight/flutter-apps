5fb2ac457aa32a57f4122eed912564346fa2787f
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-06-20 15:53:09 -0400

    Updated styles to handle different screen sizes

--- flutter_course/lib/widgets/ui_elements/title_default.dart
+++ flutter_course/lib/widgets/ui_elements/title_default.dart
@@ -7,10 +7,12 @@ class TitleDefault extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
     return Text(
       title,
+      textAlign: TextAlign.center,
       style: TextStyle(
-        fontSize: 26.0,
+        fontSize: deviceWidth > 700 ? 26.0 : 14.0,
         fontWeight: FontWeight.bold,
         fontFamily: 'Oswald',
       ),
